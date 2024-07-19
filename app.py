abd8c3ac85b63f16aa3f01354170eab3ff083ecb
marvinm2 <marvin.martens@maastrichtuniversity.nl>
2024-07-19 12:40:32 +0200

    functionality for adding multiple models, and a mock second model

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 from flask import Flask, request, render_template
 from qsprpred.models import SklearnModel
+import os
 import traceback
 
 app = Flask(__name__)
 
-model_name = 'models/P10828_RF_Model/P10828_RF_Model_meta.json'
-model = SklearnModel.fromFile(model_name)
+# Define the models directory
+MODELS_DIR = 'models'
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # List available models
+    available_models = [d for d in os.listdir(MODELS_DIR) if os.path.isdir(os.path.join(MODELS_DIR, d))]
+    return render_template('index.html', models=available_models)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         smiles = request.form.get('smiles')
+        model_name = request.form.get('model')
+        
         if not smiles:
             return render_template('index.html', error="No SMILES strings provided.")
+        if not model_name:
+            return render_template('index.html', error="No model selected.")
         
+        model_path = os.path.join(MODELS_DIR, model_name, f"{model_name}_meta.json")
+        
+        if not os.path.exists(model_path):
+            return render_template('index.html', error="Selected model does not exist.")
+        
+        model = SklearnModel.fromFile(model_path)
         smiles_list = [smile.strip() for smile in smiles.split(',')]
         predictions = model.predictMols(smiles_list)
         predictions_formatted = [f"{pred[0]:.4f}" for pred in predictions]
@@ -25,12 +38,14 @@ def predict():
         # Prepare data as a list of tuples
         data = list(zip(smiles_list, predictions_formatted))
         
-        return render_template('index.html', data=data)
+        available_models = [d for d in os.listdir(MODELS_DIR) if os.path.isdir(os.path.join(MODELS_DIR, d))]
+        return render_template('index.html', models=available_models, data=data)
     except Exception as e:
         # Print the exception to the Flask server logs
         print(f"Exception occurred: {e}")
         traceback.print_exc()
-        return render_template('index.html', error="An error occurred while processing the request.")
+        available_models = [d for d in os.listdir(MODELS_DIR) if os.path.isdir(os.path.join(MODELS_DIR, d))]
+        return render_template('index.html', models=available_models, error="An error occurred while processing the request.")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)  # Enable debug mode
