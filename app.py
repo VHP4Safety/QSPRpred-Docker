22338ba2322e0d8bdeb7f893fb3848c33f837bcf
marvinm2 <marvin.martens@maastrichtuniversity.nl>
2024-07-30 17:21:36 +0200

    corrected csv curl command functionality

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, jsonify, Response
 from qsprpred.models import SklearnModel
 import os
+import io
+import csv
 import json
 import traceback
 import pandas as pd
@@ -74,17 +76,48 @@ def predict():
 def apipredict():
     data = request.json
     smiles = data.get('smiles', [])
+    models = data.get('models', [])
     output_format = data.get('format', 'json')  # Default to JSON format
 
     if not smiles or not isinstance(smiles, list):
         return jsonify({'error': 'Invalid input: please provide a list of SMILES strings.'}), 400
 
-    predictions = model.predictMols(smiles)
-    result = [{'smiles': sm, 'prediction': pred[0]} for sm, pred in zip(smiles, predictions)]
+    if not models or not isinstance(models, list):
+        return jsonify({'error': 'Invalid input: please provide a list of model names.'}), 400
+
+    all_predictions = {}
+
+    for model_name in models:
+        model_path = os.path.join(MODELS_DIR, model_name, f"{model_name}_meta.json")
+        if not os.path.exists(model_path):
+            return jsonify({'error': f"Model {model_name} does not exist."}), 400
+
+        model = SklearnModel.fromFile(model_path)
+        predictions = model.predictMols(smiles)
+        predictions_formatted = [f"{pred[0]:.4f}" for pred in predictions]
+        all_predictions[model_name] = predictions_formatted
+
+    # Format the result
+    result = []
+    for i, smile in enumerate(smiles):
+        result_entry = {'smiles': smile}
+        for model_name in models:
+            result_entry[f'prediction ({model_name})'] = all_predictions[model_name][i]
+        result.append(result_entry)
 
     if output_format == 'text':
-        result_text = '\n'.join([f"SMILES: {sm} -> Prediction: {pred[0]:.4f}" for sm, pred in zip(smiles, predictions)])
+        result_text = '\n'.join([f"SMILES: {entry['smiles']} -> " + ", ".join([f"{model}: {pred}" for model, pred in entry.items() if model != 'smiles']) for entry in result])
         return Response(result_text, mimetype='text/plain')
+
+    if output_format == 'csv':
+        # Create an in-memory output file for CSV data
+        output = io.StringIO()
+        writer = csv.DictWriter(output, fieldnames=result[0].keys())
+        writer.writeheader()
+        writer.writerows(result)
+        output.seek(0)
+        return Response(output.getvalue(), mimetype='text/csv', headers={"Content-Disposition": "attachment;filename=predictions.csv"})
+
 
     return jsonify(result)
 
